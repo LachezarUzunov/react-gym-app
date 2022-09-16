f187d4a378be5b7ff8c197b66bc06e7204af9008
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-09-16 19:01:07 +0300

    Fetch and display single exercise details

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,6 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  exerciseOptions,
+  fetchData,
+  youtubeOptions,
+} from "../services/FetchData";
+
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
 const ExerciseDetail = () => {
-    return <div>ExerciseDetail</div>;
-  };
-  
-  export default ExerciseDetail;
-  
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtuve-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos}/>
+      <SimilarExercises />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
